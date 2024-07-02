16f55922e0f259ada84674891d885a8cc61cff4c
Jerwin Uy <uy.jerwin1984@gmail.com>
2024-07-02 09:15:54 +0800

    use hibernate jpa model

--- main/src/main/java/com/ph/juy/ls/repository/specifications/SpeechSpecification.java
+++ main/src/main/java/com/ph/juy/ls/repository/specifications/SpeechSpecification.java
@@ -1,6 +1,7 @@
 package com.ph.juy.ls.repository.specifications;
 
 import com.ph.juy.ls.repository.entity.SpeechEntity;
+import com.ph.juy.ls.repository.entity.SpeechEntity_;
 import org.springframework.data.jpa.domain.Specification;
 
 import java.time.LocalDate;
@@ -8,19 +9,19 @@ import java.time.LocalDate;
 public class SpeechSpecification {
 
     public static Specification<SpeechEntity> author(final String author) {
-        return (root, query, builder) -> builder.like(root.get("author"), "%" + author + "%");
+        return (root, query, builder) -> builder.like(root.get(SpeechEntity_.AUTHOR), "%" + author + "%");
     }
 
     public static Specification<SpeechEntity> content(final String content) {
-        return (root, query, builder) -> builder.like(root.get("content"), "%" + content + "%");
+        return (root, query, builder) -> builder.like(root.get(SpeechEntity_.CONTENT), "%" + content + "%");
     }
 
     public static Specification<SpeechEntity> keyword(final String keyword) {
-        return (root, query, builder) -> builder.like(root.get("keyword"), "%" + keyword + "%");
+        return (root, query, builder) -> builder.like(root.get(SpeechEntity_.KEYWORD), "%" + keyword + "%");
     }
 
     public static Specification<SpeechEntity> authorDate(final LocalDate authorDate) {
-        return (root, query, builder) -> builder.equal(root.get("authorDate"), authorDate);
+        return (root, query, builder) -> builder.equal(root.get(SpeechEntity_.AUTHOR_DATE), authorDate);
     }
 
 }
